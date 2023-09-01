b27a21cf2911d54d002413e33701c0216b810cf9
Ian Mugambi <linksian63@gmail.com>
2023-09-01 07:41:14 +0300

    feat: init repository

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+export default bundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+});
